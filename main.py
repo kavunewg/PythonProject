c9b5345a177bdf3383b5138b128e005b7362c1d4
Евгений <kavun.ewg@yandex.ru>
2025-04-28 16:17:23 +0300

    init

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_card_number, get_mask_account
 from src.widget import mask_card_or_chet, get_date
+from src.processing import filter_by_state, sort_by_date
 
 if __name__ == "__main__":
     # Проверка домашки 9.1 Poetry. Оформление кода
@@ -22,3 +23,8 @@ if __name__ == "__main__":
     print()
     print(get_date("2024-03-11T02:26:18.671407"))
     print(get_date("2023-12-25T15:30:00.000000"))
+    #Проверка домашки 10.1 Продвинутый Git
+    print()
+    print("Домашнее задание 10.1 Продвинутый Git")
+    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,15 @@ def filter_by_state(data, state='EXECUTED'):
     :return: Новый список словарей, соответствующих указанному значению ключа 'state'.
     """
     return [item for item in data if item.get('state') == state]
+
+
+from datetime import datetime
+def sort_by_date(data, descending=True):
+    """
+    Сортирует список словарей по значению ключа 'date'.
+
+    :param data: Список словарей для сортировки.
+    :param descending: Параметр, указывающий порядок сортировки (по умолчанию True - убывание).
+    :return: Новый список словарей, отсортированный по дате.
+    """
+    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=descending)
