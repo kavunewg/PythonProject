ecdd8a3d640cc864cede90006e7c2ced60f70d0a
Евгений <kavun.ewg@yandex.ru>
2025-04-29 20:52:16 +0300

    rename README.md

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
-def filter_by_state(data, state='EXECUTED'):
+from datetime import datetime
+
+
+def filter_by_state(data, state="EXECUTED"):
     """
     Фильтрует список словарей по значению ключа 'state'.
 
@@ -6,10 +9,9 @@ def filter_by_state(data, state='EXECUTED'):
     :param state: Значение ключа 'state', по которому будет производиться фильтрация (по умолчанию 'EXECUTED').
     :return: Новый список словарей, соответствующих указанному значению ключа 'state'.
     """
-    return [item for item in data if item.get('state') == state]
+    return [item for item in data if item.get("state") == state]
 
 
-from datetime import datetime
 def sort_by_date(data, descending=True):
     """
     Сортирует список словарей по значению ключа 'date'.
@@ -18,4 +20,4 @@ def sort_by_date(data, descending=True):
     :param descending: Параметр, указывающий порядок сортировки (по умолчанию True - убывание).
     :return: Новый список словарей, отсортированный по дате.
     """
-    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=descending)
+    return sorted(data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=descending)
--- src/widget.py
+++ src/widget.py
@@ -1,25 +1,26 @@
 def mask_card_or_chet(card_or_chet_number: int | str) -> str:
     """Маскирует номер карты или номер счета"""
     parts = card_or_chet_number.split()
-    name = ' '.join(parts[:-1])
+    name = " ".join(parts[:-1])
     number = parts[-1]
 
     if name.lower().startswith("счет"):
         # Если это счет, зашифровываем последние 4 цифры
-        encrypted_number = '**' + number[-4:]
+        encrypted_number = "**" + number[-4:]
     else:
         # Если это карта, зашифровываем все, кроме первых 6 и последних 4 цифр
-        encrypted_number = number[:4] + ' ' + number[5:7] + '**' + ' ' + '****' + ' ' + number[-4:]
+        encrypted_number = number[:4] + " " + number[5:7] + "**" + " " + "****" + " " + number[-4:]
 
     return f"{name} {encrypted_number}"
 
 
 def get_date(date_string):
+    """Форматирует полученную строку в понятный формат даты"""
     # Разделяем строку по символу 'T' и берем первую часть (дату)
-    date_part = date_string.split('T')[0]
+    date_part = date_string.split("T")[0]
 
     # Разделяем дату на год, месяц и день
-    year, month, day = date_part.split('-')
+    year, month, day = date_part.split("-")
 
     # Формируем строку в нужном формате
     formatted_date = f"{day}.{month}.{year}"
